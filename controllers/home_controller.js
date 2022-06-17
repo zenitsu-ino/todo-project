0165b302f3f0b138e51d1c8ad3616c1f6d5b1da3
Vinay Singh <vinaysingh@Vinays-MacBook-Air.local>
2022-06-17 23:46:51 +0530

    html css js & connected to mongodb

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>lets start the project</h1>');
+    return res.render('home', {
+        title: "Home"
+    });
 }
+
+// module.exports.actionName = function(req, res){}
--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
-// set up the view engine
+// setting view engine as ejs
 app.set('view engine','ejs');
+
+// setting path for views
 app.set('views','./views');
 
+// to use static files, present in assets directory
+app.use(express.static('assets'));
+
+// server configurations...
 app.listen(port, function(err){
     if (err){
         // console.log('Error: ', err);
